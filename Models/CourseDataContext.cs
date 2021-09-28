f8a141fbdb7c6becd0178e879c4e884bc37b9f60
Oscar-Grey <greyoscar00@gmail.com>
2021-09-28 15:16:08 +1300

    creating pages needed

---
+++ Models/CourseDataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNET_GROUP_PROJECT.Models
+{
+    public class CourseDataContext : DbContext
+    {
+        public CourseDataContext(DbContextOptions<CourseDataContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
