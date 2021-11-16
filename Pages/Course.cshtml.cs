76062509bd5e7bb28d9934bc127f5946847168ee
Keaton96 <keatonwinkworth@outlook.co.nz>
2021-11-16 15:23:00 +1300

    Congrats, it a Timetable!!

--- Pages/Course.cshtml.cs
+++ Pages/Course.cshtml.cs
@@ -14,5 +14,16 @@ namespace ASPNET_GROUP_PROJECT.Pages
         {
             Message = "Welcome to the Course page. Here you can view all the courses you enrolled in.";
         }
+
+        //public class Class
+        //{
+        //    public String Name { get; set; }
+        //    public String Location { get; set; }
+        //    public String Lecturer { get; set; }
+        //    public int Weeksattending { get; set; }
+        //}
+
+        //**Nothing to see here**
     }
+
 }
